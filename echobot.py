db3090b1187923568d9451c9a450c2c9b9ba6049
tuychiyevkm <tmkomron@gmail.com>
2025-08-07 16:00:35 +0500

    done

--- echobot.py
+++ echobot.py
@@ -1,16 +1,51 @@
-from pprint import pprint
+import time
 import requests
 from settings import TOKEN
 
-url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+url_get_updates = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+url_send_message = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
+try:
+    with open('last_update.txt', 'r') as f:
+        last_update_id = int(f.read())
+except FileNotFoundError:
+    last_update_id = 0
 
-r=requests.get(url)
-data = r.json()
+while True:
+    params = {
+        'offset': last_update_id + 1
+    }
 
-result = data["result"]
+    r = requests.get(url_get_updates, params=params)
+    data = r.json()
+    print(data)
 
-last_update = result[-1]
+    result = data.get('result', [])
 
+    for update in result:
+        msg = update.get('message', {})
+        user = msg.get('from', {})
+        text = msg.get('text', '')
+        chat_id = user.get('id')
 
-pprint(last_update)
+        if not chat_id or not text:
+            continue
+
+        if text == '/start':
+            params = {
+                'chat_id': chat_id,
+                'text': 'salom echo botga xush kelibsiz\nbu bot text yozsangiz echo qiladi.'
+            }
+        else:
+            params = {
+                'chat_id': chat_id,
+                'text': text
+            }
+
+        requests.get(url=url_send_message, params=params)
+
+        last_update_id = update['update_id']
+        with open('last_update.txt', 'w') as f:
+            f.write(f'{last_update_id}')
+
+    time.sleep(1)
